de32af87ff53d5eb3aa19becd55526c512cabffa
Cindy Sun <cindy@metalpay.co>
2021-03-04 18:35:38 +0000

    WEB-749 - Pull all templates from collection and display lowest price of each

---
+++ services/templates.ts
@@ -0,0 +1,51 @@
+import NodeFetch from '../utils/node-fetch';
+
+export type SchemaFormat = {
+  name: string;
+  type: string;
+};
+
+export type Schema = {
+  schema_name: string;
+  format: SchemaFormat[];
+  created_at_block: string;
+  created_at_time: string;
+};
+
+export type Collection = {
+  collection_name: string;
+  name: string | null;
+  img: string | null;
+  author: string;
+  allow_notify: boolean;
+  authorized_accounts: string[];
+  notify_accounts: string[] | [];
+  market_fee: number;
+  created_at_block: string;
+  created_at_time: string;
+};
+
+type ImmutableData = {
+  name: string;
+  image: string;
+  series: number;
+};
+
+export type Template = {
+  contract: string;
+  template_id: string;
+  is_transferable: boolean;
+  is_burnable: boolean;
+  issued_supply: string;
+  max_supply: string;
+  collection: Collection;
+  schema: Schema;
+  immutable_data: ImmutableData;
+  created_at_time: string;
+  created_at_block: string;
+  name: string;
+};
+
+export const templatesApiService = new NodeFetch<Template>(
+  '/atomicassets/v1/templates'
+);
---
+++ utils/browser-fetch.ts
@@ -0,0 +1,52 @@
+import { APIResponse } from './node-fetch';
+
+interface BrowserResponse<T> {
+  success: boolean;
+  data: APIResponse<T>;
+}
+
+const request = <T, P = void>(
+  url: string,
+  body: P | null,
+  headers?: HeadersInit | undefined,
+  method?: string
+): Promise<BrowserResponse<T>> => {
+  const isValidBody = body && Object.keys(body).length > 0;
+  const isValidHeader = headers && Object.keys(headers).length > 0;
+  const originalHeaders = {
+    Accept: 'application/json',
+    'Content-Type': 'application/json',
+  };
+
+  const options: RequestInit = {
+    method: method || 'GET',
+    body: isValidBody ? JSON.stringify(body) : null,
+    headers: isValidHeader
+      ? Object.assign(originalHeaders, headers)
+      : originalHeaders,
+  };
+
+  return fetch(url, options)
+    .then((result) => {
+      return result.json();
+    })
+    .catch((e) => {
+      throw Error(e);
+    });
+};
+
+export const sendToApi = <T, P>(
+  method: string,
+  url: string,
+  body: P,
+  header?: HeadersInit | undefined
+): Promise<BrowserResponse<T>> => {
+  return request<T, P>(url, body, header, method);
+};
+
+export const getFromApi = <T>(
+  url: string,
+  header?: HeadersInit | undefined
+): Promise<BrowserResponse<T>> => {
+  return request<T>(url, null, header, 'GET');
+};
