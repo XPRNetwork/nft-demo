9f9b4462241c0696f308895c339b072d8b2a8531
Cindy Sun <cindy@metalpay.co>
2021-03-05 01:00:35 +0000

    WEB-750: Pull data for my own collection and cross check with offers

---
+++ services/offers.ts
@@ -0,0 +1,23 @@
+import NodeFetch from '../utils/node-fetch';
+import { Asset } from './assets';
+
+type Offers = {
+  contract: string;
+  offer_id: string;
+  sender_name: string;
+  recipient_name: string;
+  memo: string;
+  state: number;
+  sender_assets: Asset[];
+  recipient_assets: Asset[];
+  is_sender_contract: boolean;
+  is_recipient_contract: boolean;
+  updated_at_block: string;
+  updated_at_time: string;
+  created_at_block: string;
+  created_at_time: string;
+};
+
+export const offersApiService = new NodeFetch<Offers>(
+  '/atomicassets/v1/offers'
+);
