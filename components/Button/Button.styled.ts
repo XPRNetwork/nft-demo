55c5b9bee6d61d92a227d75c27c18d2f6669a8ec
Joanna Chen <joanna@metalpay.co>
2021-03-24 14:44:50 -0700

    Update sale creation and cancellation modal buttons

--- components/Button/Button.styled.ts
+++ components/Button/Button.styled.ts
@@ -1,20 +1,18 @@
 import styled from 'styled-components';
 
-type ButtonProps = {
-  color: string;
-  hoverColor: string;
+export interface ButtonProps {
   filled?: boolean;
   rounded?: boolean;
   fullWidth?: boolean;
-};
+}
 
 export const StyledButton = styled.button<ButtonProps>`
   padding: 8px 16px;
   margin: 12px 0;
   border-radius: ${({ rounded }) => (rounded ? '20px' : '4px')};
   border: ${({ filled }) => (filled ? 'none' : ' 1px solid #e8ecfd')};
-  background-color: ${({ filled, color }) => (filled ? color : '#ffffff')};
-  color: ${({ filled, color }) => (filled ? '#ffffff' : color)};
+  background-color: ${({ filled }) => (filled ? '#8a9ef5' : '#ffffff')};
+  color: ${({ filled }) => (filled ? '#ffffff' : '#8a9ef5')};
   cursor: pointer;
   outline: none;
   transition: 0.2s;
@@ -28,8 +26,7 @@ export const StyledButton = styled.button<ButtonProps>`
   :focus {
     opacity: 1;
     color: #ffffff;
-    background-color: ${({ filled, hoverColor }) =>
-      filled ? hoverColor : '#8a9ef5'};
+    background-color: ${({ filled }) => (filled ? '#4d5dc1' : '#8a9ef5')};
     box-shadow: 0 8px 12px -4px rgba(130, 136, 148, 0.24),
       0 0 4px 0 rgba(141, 141, 148, 0.16), 0 0 2px 0 rgba(141, 141, 148, 0.12);
   }
