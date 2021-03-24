bf37964fce6a21aeda598a180930476f299d6404
Cindy Sun <c.s.sun5@gmail.com>
2021-03-24 13:00:14 -0700

    Add Google analytics

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,8 +1,47 @@
-import Document, { DocumentContext } from 'next/document';
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+import Document, {
+  Html,
+  Head,
+  Main,
+  NextScript,
+  DocumentContext,
+} from 'next/document';
 import { ServerStyleSheet } from 'styled-components';
 
+const isProduction = process.env.NODE_ENV === 'production';
+
 export default class MyDocument extends Document {
-  // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          {isProduction && (
+            <>
+              <script
+                async
+                src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_TRACKING_ID}`}
+              />
+              <script
+                dangerouslySetInnerHTML={{
+                  __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', ${process.env.NEXT_PUBLIC_GA_TRACKING_ID});
+              `,
+                }}
+              />
+            </>
+          )}
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+
   static async getInitialProps(ctx: DocumentContext) {
     const sheet = new ServerStyleSheet();
     const originalRenderPage = ctx.renderPage;
