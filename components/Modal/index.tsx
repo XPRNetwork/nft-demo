6881de9acae274bc78d9c1bf835a791756ed0fc5
Joanna Chen <joanna@metalpay.co>
2021-03-24 15:42:03 -0700

    Fix import statement and remove unnecessary export

--- components/Modal/index.tsx
+++ components/Modal/index.tsx
@@ -1,8 +1,3 @@
-<<<<<<< HEAD
 export { ClaimBalanceModal } from './ClaimBalanceModal';
-export { CancelSaleModal, CancelAllSalesModal } from './CancelSaleModal';
-=======
-export { WithdrawModal } from './WithdrawModal';
 export { CancelSaleModal, CancelMultipleSalesModal } from './CancelSaleModal';
->>>>>>> a4edb15... Rename cancelAllSaleModal to cancelMultipleSalesModal
 export { CreateSaleModal, CreateMultipleSalesModal } from './CreateSaleModal';
