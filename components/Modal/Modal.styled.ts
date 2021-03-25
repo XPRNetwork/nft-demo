7334677a74858201924e0624b3498c35258aea82
Joe Sobo <joe@metalpay.co>
2021-03-25 16:32:58 -0700

    moved modal up

--- components/Modal/Modal.styled.ts
+++ components/Modal/Modal.styled.ts
@@ -25,6 +25,7 @@ export const Background = styled.div`
 export const ModalBox = styled(MaxWidth)`
   display: flex;
   flex-direction: column;
+  margin-bottom: 28vh;
   padding: 24px 24px 12px;
   border-radius: 8px;
   box-shadow: 0 8px 8px -4px rgba(0, 0, 0, 0.1), 0 0 4px 0 rgba(0, 0, 0, 0.08);
