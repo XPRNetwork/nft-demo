a7177b4f79dd75907b102add8b269fd1abf97d8d
Joe Sobo <joe@metalpay.co>
2021-03-29 10:54:44 -0700

    added conditional rendering

--- components/AssetFormSellPopupMenu/index.tsx
+++ components/AssetFormSellPopupMenu/index.tsx
@@ -7,19 +7,30 @@ import {
 } from './AssetFormSellPopupMenu.styled';
 import { GradientBackground } from '../NavBar/NavBar.styled';
 import { ReactComponent as Ellipsis } from '../../public/ellipsis.svg';
-import { useModalContext, MODAL_TYPES } from '../Provider';
+import {
+  useModalContext,
+  MODAL_TYPES,
+  CreateMultipleSalesModalProps,
+  CancelMultipleSalesModalProps,
+} from '../Provider';
 import { useScrollLock, useEscapeKeyClose } from '../../hooks';
 
 const AssetFormSellPopupMenu = (): JSX.Element => {
-  const { openModal } = useModalContext();
+  const { openModal, modalProps } = useModalContext();
   const [isOpen, setIsOpen] = useState<boolean>(false);
   const togglePopupMenu = () => setIsOpen(!isOpen);
   const closePopupMenu = () => setIsOpen(false);
+  const { saleIds } = modalProps as CancelMultipleSalesModalProps;
+  const { assetIds } = modalProps as CreateMultipleSalesModalProps;
   useScrollLock(isOpen);
   useEscapeKeyClose(closePopupMenu);
 
+  console.log(saleIds); //for sale
+  console.log(assetIds); //not for sale
+
   const popupMenuItems = [
     {
+      type: 'Sell',
       name: 'Mark all for sale',
       onClick: () => {
         setIsOpen(false);
@@ -27,6 +38,7 @@ const AssetFormSellPopupMenu = (): JSX.Element => {
       },
     },
     {
+      type: 'Cancel',
       name: 'Cancel all sales',
       onClick: () => {
         setIsOpen(false);
@@ -41,11 +53,18 @@ const AssetFormSellPopupMenu = (): JSX.Element => {
         <Ellipsis />
       </PopupMenuButton>
       <Menu isOpen={isOpen}>
-        {popupMenuItems.map(({ name, onClick }) => (
-          <MenuItem key={name} tabIndex={0} onClick={onClick}>
-            {name}
-          </MenuItem>
-        ))}
+        {popupMenuItems.map(({ type, name, onClick }) => {
+          if (
+            (type === 'Sell' && assetIds.length !== 0) ||
+            (type === 'Cancel' && saleIds.length !== 0)
+          ) {
+            return (
+              <MenuItem key={name} tabIndex={0} onClick={onClick}>
+                {name}
+              </MenuItem>
+            );
+          }
+        })}
       </Menu>
       <GradientBackground
         isTransparent
